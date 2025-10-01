c627fafed01861fd8addf5df4276d90db9452d9b
samet <142526344+tebberen@users.noreply.github.com>
2025-10-01 16:59:17 +0300

    Create simplestorage.sol

---
+++ simplestorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private storedData;
+    
+    event ValueChanged(uint256 newValue);
+    
+    function set(uint256 x) public {
+        storedData = x;
+        emit ValueChanged(x);
+    }
+    
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
